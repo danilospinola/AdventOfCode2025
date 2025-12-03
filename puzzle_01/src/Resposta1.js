c638babc957f8f5779a92955c2a8040044cef65e
danilospinola <102962780+danilospinola@users.noreply.github.com>
2025-12-03 15:35:46 -0300

    retirado os arquivos de RUST
    A linguagem utilizada foi js no node.js

--- puzzle_01/src/Resposta1.js
+++ puzzle_01/src/Resposta1.js
@@ -28,18 +28,18 @@ async function processarArquivoLinhaPorLinha() {
         if(linha.startsWith('R')){      /// SE A LINHA COMEÇA COM R O PROGRAMA IRÁ ENTRAR DENTRO DO IF
             const num = linha.split("R")
             console.log(num)
-            if (x+ Number(num[1]) > 99 ){
+            if (x+ Number(num[1]) > 99 ){ // LE O NUMERO DENTRO DO VETOR, SE O NUMERO FOR MAIOR QUE 99 IRÁ SUBTRAIR 100
                 x = x + Number(num[1]) - 100
-                while((x) > 99){
+                while((x) > 99){ // SUBTRAI 100 ATÉ QUE O NUMERO FIQUE MENOR OU IGUAL 99
                     x = x - 100
                 }
             }
             else{
-               x = x+ Number(num[1])
+               x = x+ Number(num[1]) // CASO NÃO FOR MAIOR QUE 99 IRÁ SOMAR O NUMERO NORMALMENTE
             }
             
             if (x == 0){
-                    cont = cont +1
+                    cont = cont +1 // ADICIONA MAIS UM NA RESPOSTA
             }
         }
 
@@ -48,17 +48,18 @@ async function processarArquivoLinhaPorLinha() {
 
         else if(linha.startsWith('L')){ /// SE A LINHA COMEÇA COM L O PROGRAMA IRÁ ENTRAR DENTRO DO IF
             const num = linha.split("L") // SEPARA A LINHA ENTRE UM VETOR
-            if (x - Number(num[1]) < 0 ){ // LE O NUMERO DENTRO DO VETOR, SE O NUMERO FOR MENOR QUE 0
+
+            if (x - Number(num[1]) < 0 ){ // LE O NUMERO DENTRO DO VETOR, SE O NUMERO FOR MENOR QUE 0 IRÁ SOMAR 100 
                 x = x - Number(num[1]) + 100
-                while((x) < 0){ // ENQUANTO 
+                while((x) < 0){ // ENQUANTO O NUMERO FOR MENOR QUE ZERO SOMA 100
                     x = x + 100
                 }
             }
             else{
-               x = x - Number(num[1])
+               x = x - Number(num[1]) // CASO A SUBTRAÇÃO NÃO FOR MENOR QUE ZERO IRÁ SUBTRAIR O NUMERO NORMALMENTE
             }
             if (x == 0){
-                cont = cont +1 
+                cont = cont +1  // ADICIONA MAIS UM NA RESPOSTA
             }
         }
             console.log(`\n✅ Resposta`, cont, ' X = ', x);
