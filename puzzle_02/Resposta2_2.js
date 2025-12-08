0c6e819194170f527bcfdfd6cbd930aa684c772a
danilospinola <102962780+danilospinola@users.noreply.github.com>
2025-12-08 18:37:27 -0300

    Exercicio 2 parte 2

--- puzzle_02/Resposta2_2.js
+++ puzzle_02/Resposta2_2.js
@@ -1,16 +1,38 @@
 const input = require('./input.js');
 const teste = require('./input.js');
-const array = teste.split(",")
+const array = input.split(",")
 let res = 0
+let check = true 
+array
 
 for(x = 0; x < array.length; x++ ){
-    numbers = array[x].split('-')
-    for(y = 0 ; Number(numbers[0])+y <= Number(numbers[1]); y++ ){
-        num = String(Number(numbers[0])+y)
 
-        
+    let numbers = array[x].split('-')
+
+    for(y = 0 ; Number(numbers[0])+y <= Number(numbers[1]); y++ ){
+        let num = String(Number(numbers[0])+y)
+        for(let size = 1; size <= num.length/2  ;size++){
+            if(num.length% size == 0){
+
+                for(let z = size; z < num.length; z+=size){
+                   if(Number(num.substring(0, size)) !=  Number(num.substring(z, z+size))){                        
+                        check = false
+                        break
+                   }
+                }
+
+                if (check){
+                    res += Number(num)
+                    console.log(num)
+                    break
+                }
+                check = true
+            }
+        } 
     }
+
 }
+
 
 console.log(res)
 
