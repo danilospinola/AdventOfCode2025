17ea0302622b7db341ffe6e6ba5cf99fe4ac1ef9
danilospinola <102962780+danilospinola@users.noreply.github.com>
2025-12-11 18:36:45 -0300

    puzzle 7_2 and 4

--- puzzle_04/src/Resposta4.js
+++ puzzle_04/src/Resposta4.js
@@ -1,1 +1,100 @@
-const batteries = require('./input.js');
+const input = require('./input.js');
+const array = input.split("\n")
+let matrixRolls = []
+let marked =0
+
+for (let x = 0; x < array.length; x++) {
+    let arrayRollRow = array[x].split("")
+    matrixRolls[x] = []
+
+    for (let y = 0; y < arrayRollRow.length; y++) {
+        matrixRolls[x].push(arrayRollRow[y])
+    }
+}
+
+
+
+for (let x = 0; x < matrixRolls.length; x++) {
+    for (let y = 0; y < matrixRolls[x].length; y++) {
+        isRoll(matrixRolls, x, y)
+    }
+}
+
+
+function isRoll(matrix,x,y) {
+    let rolos = 0
+       if (matrix[x][y] == '@') {
+            try {
+                if (matrix[x][y+1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x][y-1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x+1][y] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x+1][y+1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x+1][y-1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x-1][y] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+                        try {
+                if (matrix[x-1][y-1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+            try {
+                if (matrix[x-1][y+1] == '@') {
+                    rolos++
+                }
+            }
+            catch {
+                console.log("erro")
+            }
+        }
+    if(rolos<=3 && matrix[x][y] == '@'){
+        marked++
+    }
+}
+
+
+
+
+console.log(marked)
